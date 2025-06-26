5e2f629e70b0374aade20da78fdb2ff8e5034e35
nuhouse <68592178+nuhouse@users.noreply.github.com>
2025-06-26 17:01:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // App.js
 
+import { doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
@@ -54,7 +55,7 @@ function App() {
     try {
       // Save to Firestore: user.uid required!
       const userRef = doc(db, "users", user.uid);
-      import { doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
+      
 
 async function handleProfileUpdate(newProfile) {
   try {
