e7548b3802aa4846dd7b68827e881ff214bc5d5b
nuhouse <68592178+nuhouse@users.noreply.github.com>
2025-06-29 20:04:23 +0100

    Update index.js

--- public/functions/index.js
+++ public/functions/index.js
@@ -1,7 +1,25 @@
-const functions = require('firebase-functions');
-const { createCheckoutSession } = require('./plans/stripeCheckout');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const stripe = require("stripe")(functions.config().stripe.secret);
+const cors = require("cors")({ origin: true });
 
+admin.initializeApp();
 
-
-exports.createCheckoutSession = functions.https.onRequest(createCheckoutSession);
-
+// Create a Checkout Session for Stripe
+exports.createCheckoutSession = functions.https.onCall(async (data, context) => {
+  const { priceId, email } = data;
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "subscription",
+      line_items: [{ price: priceId, quantity: 1 }],
+      customer_email: email,
+      success_url: "https://www.lastwishbox.com/",
+      cancel_url: "https://www.lastwishbox.com/",
+    });
+    return { url: session.url };
+  } catch (err) {
+    console.error("Stripe Checkout error:", err);
+    throw new functions.https.HttpsError("internal", err.message);
+  }
+});
